8cb86e9be26d4025da63755da20e6c4427a09c66
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-14 22:37:19 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ rclone version
 mkdir -p $HOME/.config/rclone/
 wget "$CONFIG_FILE_URL" -O $HOME/.config/rclone/rclone.conf
 rclone config file
+echo "Available Remote Name"
 rclone listremotes
+echo "Currently Serving $CLOUDNAME"
 rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M

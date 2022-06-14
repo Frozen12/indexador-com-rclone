cb2e0b0542414b1e26178920e825e770b5af2f3d
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-14 21:03:38 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#author @Samucamg forked from developeranaz 
+#author @Frozen12 forked from samucamg/indexador-com-rclone
 rclone version
-mkdir /.config
-mkdir /.config/rclone/
-wget "$CONFIG_IN_URL" -O /root/.rclone.conf
+mkdir -p /.config/rclone/
+wget "$CONFIG_FLIE_URL" -O /.config/rclone/rclone.conf
 rclone listremotes
 rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
